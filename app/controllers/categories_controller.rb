dc37d0c2cc873297e0d17c7f592184b778bcde53
Taimur Azhar <taimurazhar1997@gmail.com>
2017-08-05 17:24:51 +0500

    going to add categories to live

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,35 @@
+class CategoriesController < ApplicationController
+
+  def new
+  	@category = Category.new
+  end
+
+  def create
+  	@category = Category.new(category_params)
+  	respond_to do |format|
+  		if @category.save 
+  			format.html{ redirect_to books_path}
+  		else
+  			format.html{ render :new}
+  		end
+  	end
+  end
+
+  def show 
+  	@category.find(param[:id])
+  end
+
+
+  private
+
+  	def category_params
+  		params.require(:category).permit(:name)
+  	end
+
+
+  	def set_category
+  		@category.find(param[:id])
+  	end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :categories
+
   devise_for :users
   resources :books do 
   	resources :reviews
